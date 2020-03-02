9088a4f6d4e4898c68da417c75dea1b027ecb093
Janette Kwan <kkitt@live.com>
2020-03-02 18:29:46 -0300

    user controller created

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,20 @@
+class UserController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :first_name, :last_name, :birthday, :gender)
+  end
+end
