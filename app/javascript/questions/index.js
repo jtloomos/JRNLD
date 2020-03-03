1c3d34fafa10dae7eb83d46f648a861fe3d5d388
Janette Kwan <kkitt@live.com>
2020-03-03 16:13:24 -0300

    still need to do all the AJAX for the user questions view

--- app/javascript/questions/index.js
+++ app/javascript/questions/index.js
@@ -3,10 +3,8 @@ const userQuestions = document.querySelectorAll('.list-group-item.list-group-ite
 userQuestions.forEach((question) => {
   question.addEventListener("click", (event) => {
     event.currentTarget.classList.toggle("active");
-    const questionId = event.currentTarget.id
-    <% question = UserQuestion.find(questionId) %>
-    <% question.update(status: !question.status) %>
+    // const questionId = event.currentTarget.id;
+    // <% question = UserQuestion.find(questionId) %>
+    // <% question.update(status: !question.status) %>
   });
 });
-
-
